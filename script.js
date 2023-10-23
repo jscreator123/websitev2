afd098e53a006f386d6aa37eb98a5c8a0fc3dc09
jscreator123 <64395272+jscreator123@users.noreply.github.com>
2023-10-23 01:34:40 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,26 @@
+var app = document.getElementById('js');
+
+var typewriter = new Typewriter(app, {
+    loop: false
+});
+
+
+setTimeout(function() {
+  typewriter.typeString('<strong>jscreator</strong>')
+  .start();
+}, 1000);
+
+document.querySelector('.projectss').addEventListener('scroll', function() {
+    const section = this;
+    const iframe = section.querySelector('iframe');
+    const scrollHeight = section.scrollHeight;
+    const scrollTop = section.scrollTop;
+
+    // Adjust the iframe's top position to scroll within the section
+    iframe.style.top = `${scrollTop}px`;
+
+    // If you want to stop scrolling at the end of the section, you can add a condition like this:
+    if (scrollTop + section.clientHeight >= scrollHeight) {
+        iframe.style.top = `${scrollHeight - section.clientHeight}px`;
+    }
+});
